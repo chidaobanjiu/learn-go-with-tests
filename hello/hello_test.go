7ce6e0d7a56bf0a782c6384b81391b2ccc58325a
jiangbingjie <jbjsnow@outlook.com>
2020-07-15 15:52:19 +0800

    First commit add hello

---
+++ hello/hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper() // 当测试失败时所报告的行号将在函数调用中而不是在辅助函数内部。
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
